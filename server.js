1cf613884a3a274550f0299161394f5a2a3ef16f
padma2237 <padmarampegu2237@gmail.com>
2026-06-27 08:46:53 +0530

    Use environment variable for server port
    Environment Server port changes for render

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.post('/ask', async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
